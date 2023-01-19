de9a8a83be9781951bd704a7710145be66d81aae
masauve <msauve@redhat.com>
2023-01-19 10:17:15 -0500

    changed oidc urls

--- apps/creditservice/creditservice.java
+++ apps/creditservice/creditservice.java
@@ -2,7 +2,7 @@
 
 kamel run --dev --profile=openshift --open-api=creditservice-openapi.yaml --config secret:my-datasource --build-property quarkus.datasource.camel.db-kind=postgresql  -d mvn:io.quarkus:quarkus-jdbc-postgresql -t knative.enabled=false -t route.enabled=true creditservice.java
 
-kamel run --profile=openshift --open-api=creditservice-openapi.yaml --config secret:my-datasource --build-property quarkus.datasource.camel.db-kind=postgresql  -d mvn:io.quarkus:quarkus-jdbc-postgresql -t istio.enabled=true -t knative.enabled=false creditservice.java
+kamel run --profile=openshift --open-api=file:creditservice-openapi.yaml --config secret:my-datasource --build-property quarkus.datasource.camel.db-kind=postgresql  -d mvn:io.quarkus:quarkus-jdbc-postgresql -t istio.enabled=false -t knative.enabled=false creditservice.java
 */
 
 
@@ -12,13 +12,13 @@ public class creditservice extends org.apache.camel.builder.RouteBuilder {
         from("direct:writedb")
             .unmarshal().json()
             .log("BODY: ${body}")
-            .setBody().simple("insert into transaction (CLIENT_ID, TYPE, LOCATION, AMOUNT) values ('${body[clientId]}', '${body[type]}','${body[location]}','${body[amount]}' );")
+            .setBody().simple("insert into transaction (ID, TYPE, LOCATION, AMOUNT) values ('${body[clientId]}', '${body[type]}','${body[location]}','${body[amount]}' );")
             .to("jdbc:camel")
             .setBody().simple("Success!").marshal().json();
 
         from("direct:getall")
         .log("Get all transactioins")
-        .setBody().simple("select CLIENT_ID, TYPE, LOCATION, AMOUNT from transaction")
+        .setBody().simple("select ID, TRANSACTIONCATEGORY, TRANSACTIONDESCRIPTION, AMOUNT from transaction")
         .to("jdbc:camel").marshal().json();
  }
 }
